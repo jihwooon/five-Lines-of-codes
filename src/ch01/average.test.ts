d8e34b2951a1e548fcf2013684914c1fdd4132a1
jihwooon <jihwooon@gmail.com>
2023-12-18 20:06:25 +0900

    배열의 평균을 구하는 함수를 작성하라
    함수는 한 가지만 일을 해야한다는 원칙에 따라 average 함수를 여러 함수로
    나눠 추상화로 구현했습니다

--- src/ch01/average.test.ts
+++ src/ch01/average.test.ts
@@ -1,11 +1,36 @@
-const average = (arr: number[]) => {
+const sum = (arr: number[]) => {
   let answer = 0;
 
   for (let i = 0; i < arr.length; i++) {
     answer += arr[i];
   }
-
-  return answer / arr.length;
+  return answer;
 };
 
-console.log(average([1, 2, 3, 4, 5])); // 결과값 : 3
+const size = (arr: number[]) => arr.length;
+
+const average = (arr: number[]) => sum(arr) / size(arr);
+
+describe('sum', () => {
+  context('배열이 주어지면', () => {
+    it('배열의 합을 리턴해야 한다.', () => {
+      expect(sum([1, 2, 3, 4, 5])).toBe(15);
+    });
+  });
+});
+
+describe('size', () => {
+  context('배열이 주어지면', () => {
+    it('배열의 길이를 리턴해야 한다.', () => {
+      expect(size([1, 2, 3, 4, 5])).toBe(5);
+    });
+  });
+});
+
+describe('average', () => {
+  context('배열의 합과 인덱스 길이를 나누면', () => {
+    it('평균 값을 리턴해야 한다.', () => {
+      expect(average([1, 2, 3, 4, 5])).toBe(3);
+    });
+  });
+});
