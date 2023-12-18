7aecc54df088a960ceebff12b31631237e97e219
jihwooon <jihwooon@gmail.com>
2023-12-19 01:49:07 +0900

    2에서 n까지 반복해서 소수를 구하라
    함수는 한 가지 일만 해야 한다는 원칙에 따라 함수를 추출했습니다.
    for문을 꼬리 재귀 함수를 통해 재귀 반복으로 바꿨습니다

--- src/ch01/reportPrimes.test.ts
+++ src/ch01/reportPrimes.test.ts
@@ -1,5 +1,5 @@
 const isPrime = (num: number) => {
-  if (num <= 1) {
+  if (num === 1) {
     return false;
   }
 
@@ -12,12 +12,29 @@ const isPrime = (num: number) => {
   return true;
 };
 
-const reportPrimes = (n: number) => {
-  for (let i = 2; i < n; i++) {
-    if (isPrime(i)) {
-      console.log(`${i} is prime`);
-    }
-  }
-};
+const reportIfPrimes = (i: number) => (isPrime(i) ? undefined : `${i + 1} is prime`);
 
-console.log(reportPrimes(5)); // 결과값 : 2,3
+const reportPrimes = (n: number, i: number = 2, result: string = undefined): string => (
+  i >= n ? result : reportPrimes(n, i + 1, reportIfPrimes(i))
+);
+
+describe('reportPrimes', () => {
+  context('주어진 수가 소수이면', () => {
+    it('소수인 수를 리턴 해야 한다.', () => {
+      expect(reportPrimes(1)).toBe(undefined);
+      expect(reportPrimes(2)).toBe(undefined);
+      expect(reportPrimes(3)).toBe(undefined);
+      expect(reportPrimes(4)).toBe(undefined);
+      expect(reportPrimes(5)).toBe('5 is prime');
+      expect(reportPrimes(6)).toBe(undefined);
+      expect(reportPrimes(7)).toBe('7 is prime');
+      expect(reportPrimes(8)).toBe(undefined);
+      expect(reportPrimes(9)).toBe('9 is prime');
+      expect(reportPrimes(11)).toBe('11 is prime');
+      expect(reportPrimes(10)).toBe('10 is prime');
+      expect(reportPrimes(12)).toBe(undefined);
+      expect(reportPrimes(13)).toBe('13 is prime');
+      expect(reportPrimes(14)).toBe(undefined);
+    });
+  });
+});
