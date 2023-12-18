81d6126d31023dc80b53ec3404ab1c03dc90fe52
jihwooon <jihwooon@gmail.com>
2023-12-18 22:18:05 +0900

    2차원 배열의 최소 항목 값을 구하라
    5줄 제한을 해야한다는 규칙에 따라 minimum 함수를 추출 하였습니다.

--- src/ch01/minimum.test.ts
+++ src/ch01/minimum.test.ts
@@ -1,14 +1,38 @@
-const minimum = (arr: number[][]) => {
-  let result = Number.POSITIVE_INFINITY;
-  for (let x = 0; x < arr.length; x++) {
-    for (let y = 0; y < arr[x].length; y++) {
-      if (result > arr[x][y]) {
-        result = arr[x][y];
-      }
-    }
-
-    return result;
+const min = (result: number, arr: number[][], x: number, y: number) => {
+  let answer = result;
+  if (answer > arr[x][y]) {
+    answer = arr[x][y];
   }
+
+  return answer;
 };
 
-console.log(minimum([[1, 2, 4], [1, 2, 3]])); // 결과값 : 1
+const secondLoop = (arr: number[][], x: number, result: number) => {
+  let answer = result;
+  for (let y = 0; y < arr[x].length; y++) {
+    answer = min(answer, arr, x, y);
+  }
+
+  return answer;
+};
+
+const firstLoop = (arr: number[][]) => {
+  let result = Number.POSITIVE_INFINITY;
+  for (let x = 0; x < arr.length; x++) {
+    result = secondLoop(arr, x, result);
+  }
+
+  return result;
+};
+
+const minimum = (arr: number[][]) => firstLoop(arr);
+
+describe('minimum', () => {
+  context('2차원 배열이 주어지면', () => {
+    it('최소 항목 값을 리턴해야 한다.', () => {
+      expect(minimum([[1, 2, 4], [1, 2, 3]])).toBe(1);
+      expect(minimum([[10, 5, 3], [2, 3, 2]])).toBe(2);
+      expect(minimum([[20, 3, 7], [50, 9, 5]])).toBe(3);
+    });
+  });
+});
