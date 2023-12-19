fb37180059514bf7f3fdb54a1a45000449239c47
jihwooon <jihwooon@gmail.com>
2023-12-19 16:39:10 +0900

    update 함수 메서드를 추출하라
    update 함수 내에 2가지 이상 함수 내용을 가지고 있습니다. 5줄 이하 규칙에
    따라 메서드를 추출 했습니다

--- index.ts
+++ index.ts
@@ -79,37 +79,52 @@ function moveVertical(dy: number) {
   }
 }
 
-function update() {
-  while (inputs.length > 0) {
-    const current = inputs.pop();
-    if (current === Input.LEFT) {
-      moveHorizontal(-1);
-    } else if (current === Input.RIGHT) {
-      moveHorizontal(1);
-    } else if (current === Input.UP) {
-      moveVertical(-1);
-    } else if (current === Input.DOWN) {
-      moveVertical(1);
-    }
+function updateTile(y: number, x: number) {
+  if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+    && map[y + 1][x] === Tile.AIR) {
+    map[y + 1][x] = Tile.FALLING_STONE;
+    map[y][x] = Tile.AIR;
+  } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+    && map[y + 1][x] === Tile.AIR) {
+    map[y + 1][x] = Tile.FALLING_BOX;
+    map[y][x] = Tile.AIR;
+  } else if (map[y][x] === Tile.FALLING_STONE) {
+    map[y][x] = Tile.STONE;
+  } else if (map[y][x] === Tile.FALLING_BOX) {
+    map[y][x] = Tile.BOX;
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_STONE;
-        map[y][x] = Tile.AIR;
-      } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_BOX;
-        map[y][x] = Tile.AIR;
-      } else if (map[y][x] === Tile.FALLING_STONE) {
-        map[y][x] = Tile.STONE;
-      } else if (map[y][x] === Tile.FALLING_BOX) {
-        map[y][x] = Tile.BOX;
-      }
+      updateTile(y, x);
     }
   }
+}
+
+function handleInput(current: Input) {
+  if (current === Input.LEFT) {
+    moveHorizontal(-1);
+  } else if (current === Input.RIGHT) {
+    moveHorizontal(1);
+  } else if (current === Input.UP) {
+    moveVertical(-1);
+  } else if (current === Input.DOWN) {
+    moveVertical(1);
+  }
+}
+
+function handleInputs() {
+  while (inputs.length > 0) {
+    const current = inputs.pop();
+    handleInput(current);
+  }
+}
+
+function update() {
+  handleInputs();
+  updateMap();
 }
 
 function drawMap(g: CanvasRenderingContext2D) {
