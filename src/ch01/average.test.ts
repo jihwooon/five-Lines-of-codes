24665c23404a717a9164487edad2272604b24b6a
jihwooon <jihwooon@gmail.com>
2023-12-19 12:16:15 +0900

    배열의 평균을 구하는 함수를 꼬리 재귀 함수로 변환하라
    반복문을 꼬리 재귀 함수로 변환했습니다

--- src/ch01/average.test.ts
+++ src/ch01/average.test.ts
@@ -1,20 +1,15 @@
-const sum = (arr: number[]) => {
-  let answer = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    answer += arr[i];
-  }
-  return answer;
-};
+const sumRecursive = (arr: number[], i: number = 0): number => (
+  (i === arr.length) ? 0 : arr[i] + sumRecursive(arr, i + 1)
+);
 
 const size = (arr: number[]) => arr.length;
 
-const average = (arr: number[]) => sum(arr) / size(arr);
+const average = (arr: number[]) => sumRecursive(arr) / size(arr);
 
 describe('sum', () => {
   context('배열이 주어지면', () => {
     it('배열의 합을 리턴해야 한다.', () => {
-      expect(sum([1, 2, 3, 4, 5])).toBe(15);
+      expect(sumRecursive([1, 2, 3, 4, 5])).toBe(15);
     });
   });
 });
@@ -28,7 +23,7 @@ describe('size', () => {
 });
 
 describe('average', () => {
-  context('배열의 합과 인덱스 길이를 나누면', () => {
+  context('배열이 주어지면', () => {
     it('평균 값을 리턴해야 한다.', () => {
       expect(average([1, 2, 3, 4, 5])).toBe(3);
     });
