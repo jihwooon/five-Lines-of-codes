f964ca5989611fb34a0c3ef6eae2c2898a9b2d21
leehoosgg <leehoosgg@gmail.com>
2023-12-19 18:01:20 +0900

    ch01 예제 세가지 풀이와 eslint 따옴표 해결
    ch01 sum 예제는 풀이하지 않음

--- src/ch01/reportPrimes.test.ts
+++ src/ch01/reportPrimes.test.ts
@@ -14,10 +14,12 @@ const isPrime = (num: number) => {
 
 const reportPrimes = (n: number) => {
   for (let i = 2; i < n; i++) {
-    if (isPrime(i)) {
-      console.log(`${i} is prime`);
-    }
+    if (isPrime(i)) reportIfPrime(i);
   }
 };
+
+const reportIfPrime = (i: number) => {
+  console.log(`${i} is prime`);
+}
 
 console.log(reportPrimes(5)); // 결과값 : 2,3
