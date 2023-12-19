598e3bfb6578703410648c9e22765e52a99e9464
jihwooon <jihwooon@gmail.com>
2023-12-19 12:04:38 +0900

    두 수의 합의 값을 구하라
    두 수의 합의 값을 테스트를 작성해서 통과하라

--- src/ch01/sum.test.ts
+++ src/ch01/sum.test.ts
@@ -1,5 +1,10 @@
-describe('sum', () => {
-  it('두 수가 주어지면 합의 결괏값을 리턴해야 한다.', () => {
-    expect(1 + 2).toBe(3);
+const total = (num: number, num1: number) => num + num1;
+
+describe('total', () => {
+  it('두 수의 합의 전체 수를 리턴해야 한다.', () => {
+    expect(total(1, 2)).toBe(3);
+    expect(total(2, 1)).toBe(3);
+    expect(total(3, 0)).toBe(3);
+    expect(total(4, -1)).toBe(3);
   });
 });
