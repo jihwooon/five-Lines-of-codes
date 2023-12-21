218b460b4c507335b3ff6f2e3fd993141aa8446c
LeeUijin <n2n23339@gmail.com>
2023-12-21 16:08:31 +0900

    map : create transformTile for error resolution

--- index.ts
+++ index.ts
@@ -234,7 +234,7 @@ class Up implements Input {
 }
 let playerx = 1;
 let playery = 1;
-const map: Tile[][] = [
+const rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -242,7 +242,39 @@ const map: Tile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
-
+let map: Tile[][];
+function assertExhausted(x: number): never {
+  throw new Error("Unexpected object: "+x);
+}
+function transformTile(tile: RawTile){  //RqwTile 열거형을 Til2 객체로 변환하는 새로운 메서드
+  switch(tile){
+    case RawTile.AIR: return new Air();
+    case RawTile.PLAYER: return new Player();
+    case RawTile.UNBREAKABLE: return new Unbreakable();
+    case RawTile.STONE: return new Stone();
+    case RawTile.FALLING_STONE: return new FallingStone();
+    case RawTile.BOX: return new Box();
+    case RawTile.FALLING_BOX: return new FallingBox();
+    case RawTile.KEY1: return new Key1();
+    case RawTile.LOCK1: return new Lock1();
+    case RawTile.KEY2: return new Key2();
+    case RawTile.LOCK2: return new Lock2();
+    default: assertExhausted(tile);
+  }
+}
+function transformMap(){
+  map = new Array(rawMap.length);
+  for (let y = 0; y < rawMap.length; y++){
+    map[y]= new Array(rawMap[y].length);
+    for(let x = 0; x < rawMap[y].length; x++){
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
+window.onload = () =>{
+  transformMap();
+  gameLoop();
+}
 const inputs: Input[] = [];
 
 function removeLock1() {
@@ -264,8 +296,8 @@ function removeLock2() {
   }
 }
 function moveToTile(newx: number, newy: number) {
-  map[playery][playerx] = Tile.AIR;
-  map[newy][newx] = Tile.PLAYER;
+  map[playery][playerx] = new Air();
+  map[newy][newx] = new Player();
   playerx = newx;
   playery = newy;
 }
@@ -277,7 +309,7 @@ function moveHorizontal(dx: number) {
   } else if ((map[playery][playerx + dx].isStone()
     || map[playery][playerx + dx].isBox()
     && map[playery][playerx + dx + dx].isAir()
-    && !map[playery + 1][playerx + dx].isAir()) {
+    && !map[playery + 1][playerx + dx].isAir())) {
     map[playery][playerx + dx + dx] = map[playery][playerx + dx];
     moveToTile(playerx + dx, playery);
   } else if (map[playery][playerx + dx].isKey1()) {
@@ -323,16 +355,16 @@ function updateMap(){
 function updateTile(x: number, y: number){
   if ((map[y][x].isStone() || map[y][x].isFallingStone())
   && map[y + 1][x].isAir()) {
-  map[y + 1][x] = Tile.FALLING_STONE;
-  map[y][x] = Tile.AIR;
+  map[y + 1][x] = new FallingStone();
+  map[y][x] = new Air();
 } else if ((map[y][x].isBox() || map[y][x].isFallingBox())
   && map[y + 1][x].isAir()) {
-  map[y + 1][x] = Tile.FALLING_BOX;
-  map[y][x] = Tile.AIR;
+  map[y + 1][x] = new FallingBox();
+  map[y][x] = new Air();
 } else if (map[y][x].isFallingStone) {
-  map[y][x] = Tile.STONE;
+  map[y][x] = new Stone();
 } else if (map[y][x].isFallingBox) {
-  map[y][x] = Tile.BOX;
+  map[y][x] = new Box();
 }
 }
 function createGraphics() {
@@ -352,7 +384,7 @@ function drawMap(g: CanvasRenderingContext2D){
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       colorOfTile(g, x, y);
-      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER) {
+      if (!(map[y][x].isAir() || map[y][x].isPlayer())) {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
       }
     }
