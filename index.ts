1583c46b5c90fc84639956e4eaa2588964809400
LeeUijin <n2n23339@gmail.com>
2023-12-21 16:26:28 +0900

    colorOfTile : Method Inline

--- index.ts
+++ index.ts
@@ -396,12 +396,15 @@ function drawMap(g: CanvasRenderingContext2D){
   // Draw map
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      colorOfTile(g, x, y);
+      map[y][x].color(g);
       if (!(map[y][x].isAir() || map[y][x].isPlayer())) {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
       }
     }
   }
+}
+function colorOfTile2(g: CanvasRenderingContext2D, x: number, y: number){
+  map[y][x].color(g);
 }
 
 function drawPlayer(g: CanvasRenderingContext2D){
