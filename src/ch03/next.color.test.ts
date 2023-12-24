46d805558f033de5492f8911950fbc63927a3c8f
jihwooon <jihwooon@gmail.com>
2023-12-25 00:51:16 +0900

    유사 클래스를 통합하는 리팩터링을 하라
    클래스가 유사할 때 상수 메서드를 정의하고 분기에 따라 메서드가
    실행하도록 리팩터링을 진행했습니다

--- src/ch03/next.color.test.ts
+++ src/ch03/next.color.test.ts
@@ -14,65 +14,53 @@ interface TrafficColor {
 }
 
 class Red1 implements TrafficColor {
+  constructor(private col: string) { }
+
   color(): string {
-    return 'red';
+    return this.col;
   }
 
   check(car: Car1): boolean {
-    return car.stop();
-  }
-}
-
-class Yellow1 implements TrafficColor {
-  color(): string {
-    return 'yellow';
-  }
-
-  check(car: Car1): boolean {
-    return car.stop();
-  }
-}
-
-class Green1 implements TrafficColor {
-  color(): string {
-    return 'green';
-  }
-
-  check(car: Car1): boolean {
-    return car.drive();
+    if (this.color() === 'red') {
+      return car.stop();
+    } if (this.color() === 'yellow') {
+      return car.stop();
+    } if (this.color() === 'green') {
+      return car.drive();
+    }
   }
 }
 
 const nextColor = (t: TrafficColor) => {
   if (t.color() === 'red') {
-    return new Green1();
+    return new Red1('green');
   }
 
   if (t.color() === 'yellow') {
-    return new Red1();
+    return new Red1('red');
   }
 
   if (t.color() === 'green') {
-    return new Yellow1();
+    return new Red1('yellow');
   }
 };
 
 describe('nextColor', () => {
   context('nextColor에 red가 주어지면', () => {
-    it('Green1 객체를 리턴해야 한다.', () => {
-      expect(nextColor(new Red1())).toEqual(new Green1());
+    it('Red1 객체의 green를 리턴해야 한다.', () => {
+      expect(nextColor(new Red1('red'))).toEqual(new Red1('green'));
     });
   });
 
   context('nextColor에 yellow가 주어지면', () => {
-    it('Red1 객체를 리턴해야 한다.', () => {
-      expect(nextColor(new Yellow1())).toEqual(new Red1());
+    it('Red1 객체의 red를 리턴해야 한다.', () => {
+      expect(nextColor(new Red1('yellow'))).toEqual(new Red1('red'));
     });
   });
 
   context('nextColor에 green가 주어지면', () => {
-    it('Yellow1 객체를 리턴해야 한다.', () => {
-      expect(nextColor(new Green1())).toEqual(new Yellow1());
+    it('Red1 객체의 yellow를 리턴해야 한다.', () => {
+      expect(nextColor(new Red1('green'))).toEqual(new Red1('yellow'));
     });
   });
 });
