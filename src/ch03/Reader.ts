ed8d8820c14ee4fe326d5a14f6496ee9479f5f90
jihwooon <jihwooon@gmail.com>
2023-12-25 16:06:08 +0900

    순수 조건 사용을 해서 리팩터링을 하라

--- src/ch03/Reader.ts
+++ src/ch03/Reader.ts
@@ -1,19 +1,40 @@
+class Cacher<T> {
+  private data: T;
+
+  constructor(private mutator: () => T) {
+    this.data = this.mutator();
+  }
+
+  get() {
+    return this.data;
+  }
+
+  next() {
+    this.data = this.mutator();
+  }
+}
+
 class Reader {
   private data: string[];
 
   private current: number;
 
-  readLine() {
+  nextLine() {
     this.current++;
+  }
+
+  readLine() {
     return this.data[this.current] || null;
   }
 }
 
 const run = () => {
-  const br = new Reader();
-  let line: string | null;
-
-  while ((line = br.readLine()) !== null) {
-    return line;
+  const tmpBr = new Reader();
+  const br = new Cacher(() => tmpBr.readLine());
+  for (;br.get() !== null; br.next()) {
+    const line = br.get();
+    console.log(line);
   }
 };
+
+run();
