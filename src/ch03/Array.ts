b1008dedd6d536e0fe39cb23c9b314b454d9c427
jihwooon <jihwooon@gmail.com>
2023-12-26 18:47:06 +0900

    전략 패턴을 도입하라
    전략 패턴을 사용해서 메서드를 클래스로 추가해 변경 가능하도록 리팩터링을 진행했습니다

--- src/ch03/Array.ts
+++ src/ch03/Array.ts
@@ -1,23 +1,55 @@
 class ArrayMinimum {
-  constructor(private accumulator: number) { }
+  private processor: MinimumProcessor;
+
+  constructor(accumulator: number) {
+    this.processor = new MinimumProcessor(accumulator);
+  }
 
   process(arr: number[]) {
     for (let i = 0; i < arr.length; i++) {
-      if (this.accumulator > arr[i]) {
-        this.accumulator = arr[i];
-      }
-      return this.accumulator;
+      this.processor.processElement(arr[i]);
     }
+    return this.processor.getAccumulator();
   }
 }
 
 class ArraySum {
-  constructor(private accumulator: number) { }
+  private processor: SumProcessor;
+
+  constructor(accumulator: number) {
+    this.processor = new SumProcessor(accumulator);
+  }
 
   process(arr: number[]) {
     for (let i = 0; i < arr.length; i++) {
-      this.accumulator += arr[i];
+      this.processor.processElement(i);
     }
+    return this.processor.getAccumulator();
+  }
+}
+
+class MinimumProcessor {
+  constructor(private accumulator: number) { }
+
+  getAccumulator() {
     return this.accumulator;
+  }
+
+  processElement(e: number) {
+    if (this.accumulator > e) {
+      this.accumulator = e;
+    }
+  }
+}
+
+class SumProcessor {
+  constructor(private accumulator: number) { }
+
+  getAccumulator() {
+    return this.accumulator;
+  }
+
+  processElement(e: number) {
+    this.accumulator += e;
   }
 }
