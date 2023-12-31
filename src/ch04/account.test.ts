6058bf5c10f851300bc658fcc9a9fa63a22c6130
jihwooon <jihwooon@gmail.com>
2023-12-31 11:17:46 +0900

    순서를 강제화 하라

--- src/ch04/account.test.ts
+++ src/ch04/account.test.ts
@@ -1,27 +1,28 @@
 import db from '../database';
 
-class Account {
-  private deposit(to: string, amount: number) {
+class Transfer {
+  constructor(from: string, private amount: number) {
+    this.depositHelper(from, -this.amount);
+  }
+
+  private depositHelper(to: string, amount: number) {
     const { id } = db.account.find((v) => v.owner === to);
     const accountId = db.account[id - 1];
 
     return accountId.amount + amount;
   }
 
-  transfer(from: string, to: string, amount: number) {
-    const fromAccount = this.deposit(from, -amount);
-    const toAccount = this.deposit(to, amount);
-
-    return `${from}님 잔액은 ${fromAccount}이고 ${to}님 잔액은 ${toAccount} 입니다.`;
+  deposit(to: string) {
+    return this.depositHelper(to, this.amount);
   }
 }
 
 describe('account', () => {
   context('보내는 사람과 받은 사람, 송금 금액을 입력하면', () => {
     it('보낸 사람 잔액과 받은 사람 잔액을 출력해야 한다.', () => {
-      const account = new Account();
+      const account = new Transfer('김하준', 5000);
 
-      expect(account.transfer('김하준', '홍길동', 5000)).toBe('김하준님 잔액은 5000이고 홍길동님 잔액은 20000 입니다.');
+      expect(account.deposit('홍길동')).toBe(20000);
     });
   });
 });
