6068dff7e2d0f37a7cdc76818d6509808ed28d3f
leehoosgg <leehoosgg@gmail.com>
2024-01-02 15:19:02 +0900

    Array.ts 전략 패턴 적용
    MinimumProcessor, SumProcessor 클래스 분리하여 컴포지션

--- src/ch03/Array.ts
+++ src/ch03/Array.ts
@@ -1,23 +1,41 @@
 class ArrayMinimum {
-  constructor(private accumulator: number) { }
-
+  private processor: MinimumProcessor
+  constructor(private accumulator: number) { 
+    this.processor = new MinimumProcessor(accumulator);
+  }
   process(arr: number[]) {
-    for (let i = 0; i < arr.length; i++) {
-      if (this.accumulator > arr[i]) {
-        this.accumulator = arr[i];
-      }
-      return this.accumulator;
-    }
+    for (let i = 0; i < arr.length; i++) this.processor.processElement(arr[i]);
+    return this.processor.getAccumulator();
   }
 }
 
 class ArraySum {
-  constructor(private accumulator: number) { }
+  constructor(private processor: SumProcessor) {
+    this.processor = new SumProcessor(accumulator);
+  }
 
   process(arr: number[]) {
-    for (let i = 0; i < arr.length; i++) {
-      this.accumulator += arr[i];
-    }
+    for (let i = 0; i < arr.length; i++) this.processor.processElement(arr[i]);
+    return this.processor.getAccumulator();
+  }
+}
+
+class MinimumProcessor {
+  constructor(private accumulator: number) { }
+  getAccumulator() {
     return this.accumulator;
+  }
+  processElement(e: number) {
+    if (this.accumulator > e) this.accumulator = e;
+  }
+}
+
+class SumProcessor {
+  constructor(private accumulator: number) { }
+  getAccumulator() {
+    return this.accumulator;
+  }
+  processElement(e: number) {
+    this.accumulator += e;
   }
 }
